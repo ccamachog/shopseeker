f9dd81c687331ae03d671715a9ad97e6ee52efc9
Carlos Camacho <ccamachog@gmail.com>
2015-12-19 17:50:33 +0100

    Upload body for begin the project

--- app/index.jsx
+++ app/index.jsx
@@ -19,24 +19,3 @@ function main() {
         <Provider store={store}><App /></Provider>
         , app);
 }
-
-/*import './main.css';
- import store from './stores/AppStore.js';
- import { create, update, remove, attachToLane, detachFromLane } from './actions/LaneActions';
-
- main();
-
- function main() {
- let mystore = store;
- console.log(mystore.getState());
- // Every time the state changes, log it
- let unsubscribe = mystore.subscribe(() =>{
- debugger;
- let id=mystore.getState().lanes.get(0).id;
- mystore.dispatch(attachToLane(id,4));
- //mystore.dispatch(remove(id));
-
- })
- mystore.dispatch(create());
-
- }*/
